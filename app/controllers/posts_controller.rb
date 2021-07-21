0c624c9f67e087e3e6666818cdeb2415666ee029
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-21 12:36:12 -0500

    commit tree e914db734e040b46106e3e169c10bef9893c9d85

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,11 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    if params[:autho_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found"
+    else
+    @post = Post.new(author_id: params[:author_id])
+    end
   end
 
   def create
@@ -33,12 +37,23 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    if params[:author_id]
+      @author = Author.find_by(id: params[:id])
+      if !@author
+        redirect_to authors_path, alert: "Author not found"
+      else
+        @post = @author.posts.find_by(id: parmas[:id])
+        redirect_to author_post_path(@post), alert: "Post not found" if !@post
+      end
+    
+    else
+      @post = Post.find(params[:id])
+    end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,10 @@
 module PostsHelper
+    def author_id_field(post)
+        if post.author.nil?
+            select_tag "post[author_id]", options_for_collection_select(Author.all, :id, :name)
+        else
+            hidden_field_tag "post[author_id]", post.author_id
+        end
+    end
   
 end
